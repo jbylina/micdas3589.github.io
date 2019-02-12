b8d32ab969aaa4993054ac620d860ec83bf47ec9
Michał Daszczuk <michaldaszczuk@gmail.com>
2019-02-12 23:39:10 +0100

    Proof of concept with firebase auth.

---
+++ firebaseInit.js
@@ -0,0 +1,9 @@
+var config = {
+    apiKey: "AIzaSyAgWqG7KgonjUTqIM-DULusDVw2tRd1oH4",
+    authDomain: "testapp-e6baa.firebaseapp.com",
+    databaseURL: "https://testapp-e6baa.firebaseio.com",
+    projectId: "testapp-e6baa",
+    storageBucket: "testapp-e6baa.appspot.com",
+    messagingSenderId: "538703493876"
+  };
+  firebase.initializeApp(config);
